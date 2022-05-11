7165147815381f0033ff881aaac7b4e8b1f0b121
Sushrut1101 <guptasushrut@gmail.com>
2022-05-12 00:20:41 +0530

    Add some Messages
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- push.sh
+++ push.sh
@@ -8,6 +8,8 @@ GIT_USER="$(git config --get user.name)"
 GIT_ORG="$(echo $TARGET | cut -d / -f4)"
 repo="$(echo $TARGET | cut -d / -f5 | sed 's/.git//g')"
 
+echo "Creating the Repository..."
+sleep 1
 if [[ "${GIT_ORG}" == "${GIT_USER}" ]]; then
 	curl -s -X POST -H "Authorization: token ${TOKEN}" -d '{"name": "'"${repo}"'"}' "https://api.github.com/user/repos" >/dev/null 2>&1
 else
@@ -16,6 +18,9 @@ fi
 
 # Commit Loops
 if [[ "${COMMIT_LOOPS}" != "None" && ! -z "$COMMIT_LOOPS" ]]; then
+echo "Commit Loops Found"
+sleep 1
+echo
 for i in $COMMIT_LOOPS
 	do
 		git push -u origin HEAD~${i}:refs/heads/${TARGET_BRANCH}
@@ -23,4 +28,7 @@ for i in $COMMIT_LOOPS
 fi
 
 # Final Push
+echo "Pushing the Repo..."
+sleep 1
+printf "Repo: %s\nBranch: %s\n\n" $TARGET $TARGET_BRANCH
 git push -u origin $TARGET_BRANCH
